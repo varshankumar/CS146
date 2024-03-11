03831e5a0640c947e1d19ca1cc8f9791a1cf6a72
Varshan <41884234+varshankumar@users.noreply.github.com>
2024-03-10 22:33:58 -0700

    Add files via upload

--- Labs/Lab 4/lab4.py
+++ Labs/Lab 4/lab4.py
@@ -1,3 +1,13 @@
+class TreeNode(object):
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+
+        self.left = left
+
+        self.right = right
+
+
 def invertTree(self, root):
     if root ==None:
         return None
